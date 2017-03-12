dc95371550fdc9bf8a74b69da9861ef748fe3e54
Brendon <brennon.mckeever@gmail.com>
2017-03-12 16:46:10 -0500

    getting a reverse match error that I don't understand what is going on.

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
-
+@login_required
 def post_list(request):
 
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+@login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+
+@login_required
 def post_new(request):
 
     if request.method == "POST":
@@ -31,6 +35,8 @@ def post_new(request):
 
     return render(request, 'blog/post_edit.html', {'form': form})
 
+
+@login_required
 def post_edit(request, pk):
 
     post = get_object_or_404(Post, pk=pk)
