040a3f50d00301afcbe0d999fc2fbd18ef18a198
Brendon <brennon.mckeever@gmail.com>
2017-03-13 18:57:10 -0500

    completed the two additional exercises.

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,9 +77,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangogirls',
+        'USER': 'brendon',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
